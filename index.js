1de59bec8a36928e81cad5c009c803f7495f19a1
Ricardo Iván Vieitez Parra <ricardo@exactrealty.co.uk>
2021-11-14 00:26:42 +0100

    Use ajv standalone and dependency updates

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 const Ajv = require('ajv');
-const pack = require('ajv-pack');
+const pack = require('ajv/dist/standalone').default;
 const loaderUtils = require('loader-utils');
 
 module.exports = function(source, sourceMap) {
     this.cacheable();
 
-    const query = Object.assign({}, loaderUtils.getOptions(this), { sourceCode: true });
+    const query = Object.assign({}, loaderUtils.getOptions(this));
+    query.code = Object.assign(query.code || {}, { source: true });
 
     const callback = this.async();
     const ajv = new Ajv(query);
